b0128de27ac92090e70927c267fb9298fd51a068
juan <estresmo@gmail.com>
2023-07-18 16:01:16 -0400

    add .env

--- index.py
+++ index.py
@@ -12,12 +12,13 @@ def index():
 
 @sock.on('record')
 def handdle_record(data:bytes):
-    print("recording...")
+    print(len(record_chunks))
     record_chunks.append(data)
 
 @sock.on('message')
 def handle_message(msg:str):
     if msg == "stop":
+        print(record_chunks)
         completeFile = record_chunks[0]
         for record in record_chunks:
             if record != completeFile:

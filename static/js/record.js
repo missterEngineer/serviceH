56f247423674d1bbadff1fa569df8bb16f527242
juan <estresmo@gmail.com>
2023-07-24 10:46:40 -0400

    stop sharing screen when stop button press

--- static/js/record.js
+++ static/js/record.js
@@ -20,7 +20,16 @@ function createRecorder(stream, socket_endpoint) {
             socket.emit(socket_endpoint, e.data);
         }
     };
-    mediaRecorder.addEventListener("stop",()=> stopBtn.click() );
+    mediaRecorder.addEventListener("stop",()=> {
+        stopStream(stream);
+        stopBtn.click();
+    } );
     mediaRecorder.start(1000); // For every second the stream data will be stored in a separate chunk.
     return mediaRecorder;
+}
+
+function stopStream(stream) {
+    stream.getTracks().forEach((track) => {
+        track.stop();
+    });
 }

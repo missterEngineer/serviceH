1c2a053d7a955786ee0b68bf1d79617954dc7f4c
juan <estresmo@gmail.com>
2023-08-29 08:59:36 -0400

    delete old whisper code and error checking in openai api

--- whisper.py
+++ whisper.py
@@ -10,9 +10,12 @@ from datetime import datetime
 from audio import delTrash, mergeAudios, saveAudio, saveMic
 import os
 from utils import allowed_file, check_filename
-
+from openai.error import RateLimitError, InvalidRequestError
 old_chunks = {}
 from gradio_client import Client
+import time
+from pydub import AudioSegment
+
 client = Client("https://sanchit-gandhi-whisper-jax-diarization.hf.space/")
 
 whisper_models = [
@@ -45,20 +48,33 @@ def check_prompt(text):
 def transcribe(path, sid):
     openai.organization = os.getenv("OPENAI_ORG")
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    audio_file= open(path, "rb")
+    size = os.path.getsize(path)
+    segments = []
+    if size >= 24 * 1e+6: #Greater than 24 megabytes (whisper api limit)
+        minutes = 20 * 60 * 1000
+        mp3 = AudioSegment.from_mp3(path)
+        length = len(mp3)
+        for f in range(int(length/minutes)): 
+            new_data = mp3[minutes*f:minutes*(f+1)]
+            new_path = f"{path}.{f}.mp3"
+            new_data.export(new_path,format='mp3')
+            segments.append(new_path)
+    else:
+        segments.append(path)
     print("init diarization")
-    print("end diarization")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file, language="es")
-    texto = transcript.text
-    print("init text")
-    print("end text")
-    emit('response', texto, to=sid, namespace="/")
-    """segments, _ =  model.transcribe(path,"es")
-    for segment in segments:
-        if check_prompt(segment.text):
-            continue
-        txt =  segment.text
-        emit('response', txt, to=sid, namespace="/")"""
+    try:
+        for file_path in segments:
+            audio_file= open(file_path, "rb")
+            transcript = openai.Audio.transcribe("whisper-1", audio_file, language="es")
+            texto = transcript.text
+            emit('response', texto, to=sid, namespace="/")
+            if file_path != path:
+                os.remove(file_path)
+            time.sleep(20)
+    except Exception as e:
+        print(e)
+        emit('response', "Ha ocurrido un error al transcribir", to=sid, namespace="/")
+
 
 
 def transcription(whisper_model, audio, user, sid, app):
@@ -75,10 +91,18 @@ def resume(conversation, question):
     messages.append({"role":"user", "content": question + conversation})
     openai.organization = os.getenv("OPENAI_ORG")
     openai.api_key = os.getenv("OPENAI_API_KEY")
+    tokens = num_tokens_from_string((question*2) + conversation + response, "gpt-3.5-turbo")
+    if tokens <= 4000:
+        model = "gpt-3.5-turbo"
+    elif tokens <= 16000:
+        model = "gpt-3.5-turbo-16k"
+    else:
+        emit('chatResponse', "La conversación es demasiada larga", to=request.sid) 
+        return
     final_response = ""
     try:
         completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model=model,
             messages = messages,
             stream = True
         )
@@ -88,6 +112,9 @@ def resume(conversation, question):
                 msg = chunk["delta"]["content"]
                 final_response += msg
                 emit('chatResponse', msg, to=request.sid)  
+    except RateLimitError: 
+        time.sleep(25)
+        return resume(conversation, question)
     except:
         emit('chatResponse', "Ha ocurrido un error", to=request.sid) 
     emit('chatEnd', to=request.sid)
@@ -134,3 +161,11 @@ def real_time(record_chunks, mic_chunks, sid, app, realTime):
         print(seconds)
         os.remove(audio)
     #delTrash(sid)
+
+import tiktoken
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    encoding = tiktoken.encoding_for_model(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
